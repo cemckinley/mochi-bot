3ffdeb9211a53712e2c2c1ee47df6e43f2dd3f6d
Cara McKinley <cemckinley@gmail.com>
2014-01-21 10:19:10 -0800

    Add random cat image retrieval to bummer route

--- routes/basic.js
+++ routes/basic.js
@@ -39,17 +39,26 @@ var basicRoutes = {
 		},
 
 		":anything?bummer:anything2?": function(res){
-			var lols = [
-				'http://media.yourdailymedia.com/4/cat_98.gif',
-				'http://3.bp.blogspot.com/-TaxA8nFoMZI/UcF3eBfZ76I/AAAAAAAAkDM/DfQHzi4WAvM/s1600/funny-cat-gifs-055-004.gif',
-				'http://www.ohmagif.com/wp-content/uploads/2013/03/funny-cat-using-hedgehog-to-comb-its-fur.gif',
-				'http://3.bp.blogspot.com/-Oc3wDRTpeMU/UUmGMpCD6VI/AAAAAAAAgrw/ySayS_8pGcE/s1600/funny-cat-gifs-042-001.gif',
-				'http://2.bp.blogspot.com/-BtaQ8wVnU2M/UnVpuJWmW0I/AAAAAAAAO0I/FCu9edR-1Sk/s1600/funny-cat-drinking-water.gif',
-				'http://1.bp.blogspot.com/-RmCJjQU67NI/Ue-7WNBXJxI/AAAAAAAAnVY/jpvprp0qmfI/s1600/funny-cat-gifs-060-004.gif'
-			],
-			img = lols[ Math.floor( Math.random() * lols.length ) ];
+			var self = this,
+				httpOpts = {
+					host: 'thecatapi.com',
+					path: '/api/images/get?format=src&type=gif'
+				},
+				src = '';
+				/*lols = [
+					'http://media.yourdailymedia.com/4/cat_98.gif',
+					'http://3.bp.blogspot.com/-TaxA8nFoMZI/UcF3eBfZ76I/AAAAAAAAkDM/DfQHzi4WAvM/s1600/funny-cat-gifs-055-004.gif',
+					'http://www.ohmagif.com/wp-content/uploads/2013/03/funny-cat-using-hedgehog-to-comb-its-fur.gif',
+					'http://3.bp.blogspot.com/-Oc3wDRTpeMU/UUmGMpCD6VI/AAAAAAAAgrw/ySayS_8pGcE/s1600/funny-cat-gifs-042-001.gif',
+					'http://2.bp.blogspot.com/-BtaQ8wVnU2M/UnVpuJWmW0I/AAAAAAAAO0I/FCu9edR-1Sk/s1600/funny-cat-drinking-water.gif',
+					'http://1.bp.blogspot.com/-RmCJjQU67NI/Ue-7WNBXJxI/AAAAAAAAnVY/jpvprp0qmfI/s1600/funny-cat-gifs-060-004.gif'
+				],
+				img = lols[ Math.floor( Math.random() * lols.length ) ];*/
 
-			this.say(res.channel, "Chill bro. Check this out: " + img);
+			http.request(httpOpts, function(response){
+				src = response.headers.location;
+				self.say(res.channel, "Chill bro. Check this out: " + src);
+			}).end();
 		},
 
 		":anything?dumb:anything2?": function(res){
